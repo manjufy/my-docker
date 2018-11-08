03a1edbe46ac0814c22e0dc0f1c15e42016e2c21
Manjunath Reddy <manju16832003@gmail.com>
2018-11-08 10:38:59 +0800

    Nodejs docker compse

---
+++ docker-compose-nodejs/README.md
@@ -0,0 +1,3 @@
+# Nodejs Dockker Compose
+
+http://bitjudo.com/blog/2014/03/13/building-efficient-dockerfiles-node-dot-js/
---
+++ docker-compose-nodejs/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+// constants
+const PORT = 5000
+const HOST = '0.0.0.0'
+
+// app
+const app = express()
+app.get('/', (req, res) => {
+    res.send('Hurray!. Docker-Compose Works')
+})
+
+app.listen(PORT, HOST)
+console.log(`Running on http://${HOST}:${PORT}`)
